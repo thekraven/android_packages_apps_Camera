c73fea3585fa538f7c060df5d74722f4e30d8292
thekraven <savethestyle1@gmail.com>
2012-03-11 23:55:37 -0500

    fix
    Change-Id: I11487ea338984de9fc522b7281f46f70fccd74cb

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -51,6 +51,7 @@ import android.os.Message;
 import android.os.MessageQueue;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Display;
 import android.view.GestureDetector;
@@ -862,6 +863,12 @@ public class Camera extends BaseCamera implements View.OnClickListener,
                     loc = null;
                 }
             }
+			
+			long dateTaken = System.currentTimeMillis(); 
+            if (dateTaken != 0) { 
+                String datetime = DateFormat.format("yyyy:MM:dd kk:mm:ss", dateTaken).toString(); 
+                mParameters.set("exif-datetime",datetime); 
+            } 
 
             mCameraDevice.setParameters(mParameters);
 
